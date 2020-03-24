658f2f78f96140f7cd553a1291976d1935f37f26
masoume <masoumevsl198@gmail.com>
2020-03-24 15:24:23 +0430

    add name list in new fie

---
+++ src/AutoCompleteText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class AutoCompleteText extends React.Component{
+    constructor (props) {
+        super(props);
+        this.items = [
+            'Masoume',
+            'Parham',
+            'Arshia',
+            'Alisa',
+        ];
+    }
+}
