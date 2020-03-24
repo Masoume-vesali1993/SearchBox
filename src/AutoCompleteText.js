484722cf9adee1cc6ab950b4f3557287463a7b30
masoume <masoumevsl198@gmail.com>
2020-03-24 23:31:23 +0430

    show box in brooser

--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -14,22 +14,33 @@ export default class AutoCompleteText extends React.Component{
         };
     }
 
+
     onTextChanged = (e) => {
         const value = e.target.value;
-        if (value.length === 0){
-            this.setState(() => ({
-                suggestions: [],
-            }))
+        let suggestions = [];
+         if (value.length>0){
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.items.sort().filter(v => v.test(regex)); }
+            this.setState(() => ({ suggestions}));
+    }
+
+    renderSuggestions () {
+        const { suggestions } = this.state;
+        if (suggestions.length === 0 ){
+            return null;
         }
+        return (
+            <ul>
+                {this.suggestions.map((item) => <li> {item}</li>)}
+            </ul>
+        );
     }
 
     render () {
         return (
             <div>
                 <input onChange={this.onTextChanged} type="text" />
-                <ul>
-                    {this.items.map((item) => <li> {item}</li>)}
-                </ul>
+                {this.renderSuggestions}
             </div>
         )
     }
